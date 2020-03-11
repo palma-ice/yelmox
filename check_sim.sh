74ce242f2373acacf8258cde4d7a478a4feddf1a
Alexander Robinson <robinson@fis.ucm.es>
2020-03-11 10:59:18 +0100

    Modified check_sim.sh. (3)

--- check_sim.sh
+++ check_sim.sh
@@ -5,6 +5,16 @@ file_out=${fldr}/ens_err.txt
 
 echo "Processing results in: ${fldr}"
 
-for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${file_out}
+#for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${file_out}
+
+# Reset file to be empty 
+cat <<EOF > ${file_out} 
+
+# Loop over subfolders in fldr 
+for D in ${fldr}/*
+do
+    echo $D 
+    ./check_sim.x $D >> ${file_out}
+done 
 
 echo "Results written to: ${file_out}"
