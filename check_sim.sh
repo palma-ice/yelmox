3bf975351d6143284c46370f64f0cfd0e7f3a255
Alexander Robinson <robinson@fis.ucm.es>
2020-03-11 10:51:42 +0100

    Modified check_sim.sh. (2)

--- check_sim.sh
+++ check_sim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 fldr=$1
+file_out=${fldr}/ens_err.txt 
 
-for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${fldr}/ens_err.txt
+echo "Processing results in: ${fldr}"
+
+for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${file_out}
+
+echo "Results written to: ${file_out}"
