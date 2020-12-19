c0414f7c03a93739aef93b2cbc178f71392887b8
Alexander Robinson <robinson@ucm.es>
2020-12-19 19:45:32 +0100

    Added routines for PI-controller for hyster routine (not used or tested yet).

--- libs/hyster.f90
+++ libs/hyster.f90
@@ -172,7 +172,7 @@ contains
             ! that exp function doesn't explode (exp(-10)=>0.0)
             dvdt_fac = min(abs(hyst%dv_dt)/hyst%par%dv_dt_scale,10.0_wp)
             f_scale = exp(-dvdt_fac)
-            
+
             ! Get forcing rate of change in [f/1e6 a]
             hyst%df_dt = hyst%par%df_sign * ( hyst%par%df_dt_min + f_scale*(hyst%par%df_dt_max-hyst%par%df_dt_min) )
 
@@ -196,5 +196,259 @@ contains
 
     end subroutine hyster_calc_forcing
 
-  
+    
+    subroutine set_adaptive_timestep_pc(dt_new,dt,eta,eps,dtmin,dtmax,dx,pc_k,controller)
+        ! Calculate the timestep following algorithm for 
+        ! a general predictor-corrector (pc) method.
+        ! Implemented followig Cheng et al (2017, GMD)
+
+        implicit none 
+
+        real(wp), intent(OUT) :: dt_new               ! [yr]   Timestep (n+1)
+        real(wp), intent(IN)  :: dt(:)                ! [yr]   Timesteps (n:n-2)
+        real(wp), intent(IN)  :: eta(:)               ! [X/yr] Maximum truncation error (n:n-2)
+        real(wp), intent(IN)  :: eps                  ! [--]   Tolerance value (eg, eps=1e-4)
+        real(wp), intent(IN)  :: dtmin                ! [yr]   Minimum allowed timestep
+        real(wp), intent(IN)  :: dtmax                ! [yr]   Maximum allowed timestep
+        real(wp), intent(IN)  :: dx                   ! [m]
+        integer,    intent(IN)  :: pc_k                 ! pc_k gives the order of the timestepping scheme (pc_k=2 for FE-SBE, pc_k=3 for AB-SAM)
+        character(len=*), intent(IN) :: controller      ! Adaptive controller to use [PI42, H312b, H312PID]
+
+        ! Local variables
+        real(wp) :: dt_n, dt_nm1, dt_nm2          ! [yr]   Timesteps (n:n-2)
+        real(wp) :: eta_n, eta_nm1, eta_nm2       ! [X/yr] Maximum truncation error (n:n-2)
+        real(wp) :: rho_n, rho_nm1, rho_nm2
+        real(wp) :: rhohat_n 
+        real(wp) :: dt_adv 
+        real(wp) :: dtmax_now
+        real(wp) :: k_i 
+        real(wp) :: k_p, k_d 
+
+        ! Smoothing parameter; Söderlind and Wang (2006) method, Eq. 10
+        ! Values on the order of [0.7,2.0] are reasonable. Higher kappa slows variation in dt
+        real(wp), parameter :: kappa = 2.0_wp 
+        
+        ! Step 1: Save information needed for adapative controller algorithms 
+
+        ! Save dt from several timesteps (potentially more available)
+        dt_n    = max(dt(1),dtmin) 
+        dt_nm1  = max(dt(2),dtmin) 
+        dt_nm2  = max(dt(3),dtmin)
+
+        ! Save eta from several timesteps (potentially more available)
+        eta_n   = eta(1)
+        eta_nm1 = eta(2)
+        eta_nm2 = eta(3)
+
+        ! Calculate rho from several timesteps 
+        rho_nm1 = (dt_n   / dt_nm1) 
+        rho_nm2 = (dt_nm1 / dt_nm2) 
+
+        ! Step 2: calculate scaling for the next timestep (dt,n+1)
+        select case(trim(controller))
+
+            case("PI42")
+                ! Söderlind and Wang, 2006; Cheng et al., 2017
+                ! Deeper discussion in Söderlind, 2002. Note for example, 
+                ! that Söderlind (2002) recommends:
+                ! k*k_i >= 0.3 
+                ! k*k_p >= 0.2 
+                ! k*k_i + k*k_p <= 0.7 
+                ! However, the default values of Söderlind and Wang (2006) and Cheng et al (2017)
+                ! are outside of these bounds. 
+
+                ! Default parameter values 
+                k_i = 2.0_wp / (pc_k*5.0_wp)
+                k_p = 1.0_wp / (pc_k*5.0_wp)
+                
+                ! Improved parameter values (reduced oscillations)
+!                 k_i = 4.0_wp / (pc_k*10.0_wp)
+!                 k_p = 3.0_wp / (pc_k*10.0_wp)
+
+                ! Experimental parameter values (minimal oscillations, does not access largest timesteps)
+!                 k_i = 0.5_wp / (pc_k*10.0_wp)
+!                 k_p = 6.5_wp / (pc_k*10.0_wp)
+
+                ! Default parameter values
+                rho_n = calc_pi_rho_pi42(eta_n,eta_nm1,rho_nm1,eps,k_i,k_p,alpha_2=0.0_wp)
+
+            case("H312b") 
+                ! Söderlind (2003) H312b, Eq. 31+ (unlabeled) 
+                
+                rho_n = calc_pi_rho_H312b(eta_n,eta_nm1,eta_nm2,rho_nm1,rho_nm2,eps,k=real(pc_k,wp),b=8.0_wp)
+
+            case("H312PID") 
+                ! Söderlind (2003) H312PD, Eq. 38
+                ! Note: Suggested k_i =(2/9)*1/pc_k, but lower value gives more stable solution
+
+                !k_i = (2.0_wp/9.0_wp)*1.0_wp/real(pc_k,wp)
+                k_i = 0.08_wp/real(pc_k,wp)
+
+                rho_n = calc_pi_rho_H312PID(eta_n,eta_nm1,eta_nm2,eps,k_i)
+
+            case("H321PID")
+
+                k_i = 0.1  / real(pc_k,wp)
+                k_p = 0.45 / real(pc_k,wp) 
+
+                rho_n = calc_pi_rho_H321PID(eta_n,eta_nm1,eta_nm2,dt_n,dt_nm1,eps,k_i,k_p)
+                
+            case("PID1")
+
+                k_i = 0.175 
+                k_p = 0.075
+                k_d = 0.01 
+
+                rho_n = calc_pi_rho_PID1(eta_n,eta_nm1,eta_nm2,eps,k_i,k_p,k_d)
+                
+            case DEFAULT 
+
+                write(*,*) "set_adaptive_timestep_pc:: Error: controller not recognized."
+                write(*,*) "controller = ", trim(controller) 
+                stop 
+
+        end select 
+
+        ! Scale rho_n for smoothness 
+        rhohat_n = rho_n
+        !rhohat_n = min(rho_n,1.1)
+        !rhohat_n = 1.0_wp + kappa * atan((rho_n-1.0_wp)/kappa) ! Söderlind and Wang, 2006, Eq. 10
+        
+        ! Step 3: calculate the next time timestep (dt,n+1)
+        dt_new = rhohat_n * dt_n
+        
+        ! Finally, ensure timestep is within prescribed limits
+        call limit_adaptive_timestep(dt_new,dtmin,dtmax)
+
+        return 
+
+    end subroutine set_adaptive_timestep_pc
+
+    function calc_pi_rho_pi42(eta_n,eta_nm1,rho_nm1,eps,k_i,k_p,alpha_2) result(rho_n)
+
+        implicit none 
+
+        real(wp), intent(IN) :: eta_n 
+        real(wp), intent(IN) :: eta_nm1 
+        real(wp), intent(IN) :: rho_nm1 
+        real(wp), intent(IN) :: eps 
+        real(wp), intent(IN) :: k_i 
+        real(wp), intent(IN) :: k_p
+        real(wp), intent(IN) :: alpha_2 
+        real(wp) :: rho_n 
+
+        ! Söderlind and Wang, 2006; Cheng et al., 2017
+        ! Original formulation: Söderlind, 2002, Eq. 3.12:
+        rho_n   = (eps/eta_n)**(k_i+k_p) * (eps/eta_nm1)**(-k_p) * rho_nm1**(-alpha_2)
+
+        return 
+
+    end function calc_pi_rho_pi42
+
+    function calc_pi_rho_H312b(eta_n,eta_nm1,eta_nm2,rho_nm1,rho_nm2,eps,k,b) result(rho_n)
+
+        implicit none 
+
+        real(wp), intent(IN) :: eta_n 
+        real(wp), intent(IN) :: eta_nm1 
+        real(wp), intent(IN) :: eta_nm2 
+        real(wp), intent(IN) :: rho_nm1
+        real(wp), intent(IN) :: rho_nm2
+        real(wp), intent(IN) :: eps 
+        real(wp), intent(IN) :: k 
+        real(wp), intent(IN) :: b 
+        real(wp) :: rho_n 
+
+        ! Local variables 
+        real(wp) :: beta_1, beta_2, beta_3 
+        real(wp) :: alpha_2, alpha_3 
+
+        beta_1  =  1.0_wp / (k*b)
+        beta_2  =  2.0_wp / (k*b)
+        beta_3  =  1.0_wp / (k*b)
+        alpha_2 = -3.0_wp / b 
+        alpha_3 = -1.0_wp / b 
+
+        ! Söderlind (2003) H312b, Eq. 31+ (unlabeled) 
+        rho_n   = (eps/eta_n)**beta_1 * (eps/eta_nm1)**beta_2 * (eps/eta_nm2)**beta_3 &
+                            * rho_nm1**alpha_2 * rho_nm2**alpha_3 
+
+        return 
+
+    end function calc_pi_rho_H312b
+
+    function calc_pi_rho_H312PID(eta_n,eta_nm1,eta_nm2,eps,k_i) result(rho_n)
+
+        implicit none 
+
+        real(wp), intent(IN) :: eta_n 
+        real(wp), intent(IN) :: eta_nm1 
+        real(wp), intent(IN) :: eta_nm2 
+        real(wp), intent(IN) :: eps 
+        real(wp), intent(IN) :: k_i  
+        real(wp) :: rho_n 
+
+        ! Local variables 
+        real(wp) :: k_i_1, k_i_2, k_i_3
+
+        k_i_1   = k_i / 4.0_wp 
+        k_i_2   = k_i / 2.0_wp 
+        k_i_3   = k_i / 4.0_wp 
+
+        ! Söderlind (2003) H312PID, Eq. 38
+        rho_n   = (eps/eta_n)**k_i_1 * (eps/eta_nm1)**k_i_2 * (eps/eta_nm2)**k_i_3
+
+        return 
+
+    end function calc_pi_rho_H312PID
+
+    function calc_pi_rho_H321PID(eta_n,eta_nm1,eta_nm2,dt_n,dt_nm1,eps,k_i,k_p) result(rho_n)
+
+        implicit none 
+
+        real(wp), intent(IN) :: eta_n 
+        real(wp), intent(IN) :: eta_nm1 
+        real(wp), intent(IN) :: eta_nm2 
+        real(wp), intent(IN) :: dt_n 
+        real(wp), intent(IN) :: dt_nm1 
+        real(wp), intent(IN) :: eps 
+        real(wp), intent(IN) :: k_i  
+        real(wp), intent(IN) :: k_p
+        real(wp) :: rho_n 
+
+        ! Local variables 
+        real(wp) :: k_i_1, k_i_2, k_i_3
+
+        k_i_1   =   0.75_wp*k_i + 0.50_wp*k_p 
+        k_i_2   =   0.50_wp*k_i 
+        k_i_3   = -(0.25_wp*k_i + 0.50_wp*k_p)
+
+        ! Söderlind (2003) H321PID, Eq. 42
+        rho_n   = (eps/eta_n)**k_i_1 * (eps/eta_nm1)**k_i_2 * (eps/eta_nm2)**k_i_3 * (dt_n / dt_nm1)
+
+        return 
+
+    end function calc_pi_rho_H321PID
+
+    function calc_pi_rho_PID1(eta_n,eta_nm1,eta_nm2,eps,k_i,k_p,k_d) result(rho_n)
+
+        implicit none 
+
+        real(wp), intent(IN) :: eta_n 
+        real(wp), intent(IN) :: eta_nm1 
+        real(wp), intent(IN) :: eta_nm2 
+        real(wp), intent(IN) :: eps 
+        real(wp), intent(IN) :: k_i  
+        real(wp), intent(IN) :: k_p
+        real(wp), intent(IN) :: k_d
+        real(wp) :: rho_n 
+
+        ! https://www.mathematik.uni-dortmund.de/~kuzmin/cfdintro/lecture8.pdf
+        ! Page 20 (theoretical basis unclear/unknown)
+        rho_n   = (eps/eta_n)**k_i * (eta_nm1/eta_n)**k_p * (eta_nm1**2/(eta_n*eta_nm2))**k_d
+
+        return 
+
+    end function calc_pi_rho_PID1
+
 end module hyster
