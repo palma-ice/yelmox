95614faba7619e7a8d9aef2170de50ab6768a210
Alexander Robinson <robinson@ucm.es>
2021-03-15 06:59:29 +0100

    Added runylmox script, needs checking.

---
+++ config/brigit_runylmox.js
@@ -0,0 +1,43 @@
+{
+    "defaults" :
+    {
+        "jobname"       : "Yelmo",
+        "email"         : "USER@ucm.es",
+        "group"         : "", 
+        "omp"           : 0,
+        "wall"          : 24, 
+        "qos"           : "normal",
+        "partition"     : "",
+        "job_template"  : "config/brigit_submit_slurm"
+    },
+
+    "exe_aliases" :
+        {   "yelmox" : "libyelmox/bin/yelmox.x",
+            "iso"    : "libyelmox/bin/yelmox_iso.x",
+            "hyst"   : "libyelmox/bin/yelmox_hyst.x",
+            "rembo"  : "libyelmox/bin/yelmox_rembo.x"
+        },
+
+    "grp_aliases" : {},
+
+    "par_paths" : {},
+
+    "files" : [], 
+
+    "dir-special" : {},
+
+    "links" : 
+        ["input","ice_data"],
+
+    "const_paths" :
+        {   "EISMINT"  : "par/yelmo_const_EISMINT.nml",
+            "MISMIP3D" : "par/yelmo_const_MISMIP3D.nml"
+        },
+
+    "const_path_default" : "par/yelmo_const_Earth.nml",
+    
+    "job_queues" :
+        {  "normal" :
+            {   "wall" : 10000 }
+        }
+}
---
+++ config/runylmox.js
@@ -0,0 +1,49 @@
+{
+    "defaults" :
+    {
+        "jobname"       : "Yelmo",
+        "email"         : "USER@pik-potsdam.de",
+        "group"         : "anthroia", 
+        "omp"           : 0,
+        "wall"          : 24, 
+        "qos"           : "priority",
+        "partition"     : "haswell",
+        "job_template"  : "config/pik_submit_slurm"
+    },
+
+    "exe_aliases" :
+        {   "yelmox" : "libyelmox/bin/yelmox.x",
+            "iso"    : "libyelmox/bin/yelmox_iso.x",
+            "hyst"   : "libyelmox/bin/yelmox_hyst.x",
+            "rembo"  : "libyelmox/bin/yelmox_rembo.x"
+        },
+
+    "grp_aliases" : {},
+
+    "par_paths" : {},
+
+    "files" : [], 
+
+    "dir-special" : {},
+
+    "links" : 
+        ["input","ice_data"],
+
+    "const_paths" :
+        {   "EISMINT"  : "par/yelmo_const_EISMINT.nml",
+            "MISMIP3D" : "par/yelmo_const_MISMIP3D.nml"
+        },
+
+    "const_path_default" : "par/yelmo_const_Earth.nml",
+
+    "job_queues" :
+        {   "priority" :
+            {   "wall" : 24  },
+            "short" :
+            {   "wall" : 24  },
+            "medium" :
+            {   "wall" : 168 },
+            "long" :
+            {   "wall" : 720 }
+        }
+}
