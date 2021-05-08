2f1ce7c3c148082dd6605f2ca388c683a5418bfa
Alexander Robinson <robinson@ucm.es>
2021-05-08 14:44:02 +0200

    dimarray => xarray.

--- config/Makefile_yelmox.mk
+++ config/Makefile_yelmox.mk
@@ -49,7 +49,7 @@ $(objdir)/snapclim.o: $(libdir)/snapclim.f90 $(objdir)/nml.o $(objdir)/ncio.o
 $(objdir)/stommel.o: $(libdir)/stommel.f90 $(objdir)/yelmo_defs.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 
-$(objdir)/dimarray.o: $(libdir)/dimarray.f90 $(objdir)/nml.o $(objdir)/ncio.o
+$(objdir)/xarray.o: $(libdir)/xarray.f90 $(objdir)/nml.o $(objdir)/ncio.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 
 # insol library 
@@ -116,5 +116,5 @@ yelmox_libs = 			$(objdir)/basal_hydrology.o \
 					    $(objdir)/smb_pdd.o \
 					    $(objdir)/smbpal.o \
 					    $(objdir)/snapclim.o \
-					    $(objdir)/dimarray.o
+					    $(objdir)/xarray.o
 
