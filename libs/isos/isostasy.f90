cf2ee40baeeae5409c407c55b6ef2dc084f2ee12
Alexander Robinson <robinson@ucm.es>
2022-10-10 21:35:06 +0200

    isostasy: added gaussian smoothing function and skeleton for new isos_set_tau function.

--- libs/isos/isostasy.f90
+++ libs/isos/isostasy.f90
@@ -26,10 +26,10 @@ module isostasy
     type isos_param_class 
         integer             :: method           ! Type of isostasy to use
         character(len=512)  :: fname_kelvin     ! File containing precalculated zero-order Kelvin function values
-        real(wp)          :: dt               ! [yr] Timestep to recalculate bedrock uplift rate
-        real(wp)          :: tau              ! [yr] Asthenospheric relaxation constant
-        real(wp)          :: DL               ! [N-m] lithosphere flexural rigidity (D_lith)
-        real(wp)          :: RL               ! [m] radius of relative stiffness (R_lith)
+        real(wp)          :: dt                 ! [yr] Timestep to recalculate bedrock uplift rate
+        real(wp)          :: tau                ! [yr] Asthenospheric relaxation constant
+        real(wp)          :: D_lith             ! [N-m] lithosphere flexural rigidity
+        real(wp)          :: R_lith             ! [m] radius of relative stiffness (R_lith)
         
         ! Internal parameters 
 
@@ -123,7 +123,7 @@ contains
 
         ! Load lithospheric table of Kelvin function values 
         call read_tab_litho(isos%now%we,filename=trim(isos%par%fname_kelvin), &
-                            RL=isos%par%RL,DL=isos%par%DL,dx=dx,dy=dx)
+                            RL=isos%par%R_lith,DL=isos%par%D_lith,dx=dx,dy=dx)
 
         write(*,*) "isos_init:: range(WE):  ", minval(isos%now%we),     maxval(isos%now%we)
 
@@ -149,10 +149,10 @@ contains
         isos%now%dzbdt    = 0.0 
 
         ! Store initial values of parameters
-        isos%now%tau = isos%par%tau 
         isos%now%He_lith = 1.0_wp   ! ajr: to do!
-        isos%now%D_lith  = 1.0_wp   ! ajr: to do!
-
+        isos%now%D_lith  = isos%par%D_lith
+        isos%now%tau = isos%par%tau 
+        
 
         ! Initialize the charge field to match reference topography
         call init_charge(isos%now%charge,H_ice_ref,z_bed_ref,z_sl_ref,isos%par%lbloc)
@@ -296,8 +296,8 @@ contains
         call nml_read(filename,"isostasy","fname_kelvin",   par%fname_kelvin,   init=init_pars)
         call nml_read(filename,"isostasy","dt",             par%dt,             init=init_pars)
         call nml_read(filename,"isostasy","tau",            par%tau,            init=init_pars)
-        call nml_read(filename,"isostasy","D_lith",         par%DL,             init=init_pars)
-        call nml_read(filename,"isostasy","R_lith",         par%RL,             init=init_pars)
+        call nml_read(filename,"isostasy","D_lith",         par%D_lith,         init=init_pars)
+        call nml_read(filename,"isostasy","R_lith",         par%R_lith,             init=init_pars)
         
         return
 
@@ -364,6 +364,163 @@ contains
 
     end subroutine isos_deallocate
 
+
+    subroutine isos_set_tau(tau,tau_values,mask_values,mask,dx,sigma)
+        ! Impose multiple tau values according to the corresponding
+        ! locations provided in a mask. 
+        ! Additionally impose Gaussian smoothing via the
+        ! smoothing radius sigma 
+
+        implicit none 
+
+        real(wp), intent(OUT) :: tau(:,:) 
+        real(wp), intent(IN)  :: tau_values(:)
+        real(wp), intent(IN)  :: mask_values(:)
+        real(wp), intent(IN)  :: mask(:,:) 
+        real(wp), intent(IN)  :: dx 
+        real(wp), intent(IN)  :: sigma 
+
+
+
+        ! Apply Gaussian smoothing as desired
+        !call smooth_gauss_2D(tau,dx=dx,sigma=sigma)
+        
+        return
+
+    end subroutine isos_set_tau
+
+    subroutine smooth_gauss_2D(var,dx,sigma,mask_apply,mask_use)
+        ! Smooth out a field to avoid noise 
+        ! mask_apply designates where smoothing should be applied 
+        ! mask_use   designates which points can be considered in the smoothing filter 
+
+        implicit none
+
+        real(wp),   intent(INOUT) :: var(:,:)      ! [nx,ny] 2D variable
+        real(wp),   intent(IN)    :: dx 
+        real(wp),   intent(IN)    :: sigma  
+        logical,    intent(IN), optional :: mask_apply(:,:) 
+        logical,    intent(IN), optional :: mask_use(:,:) 
+
+        ! Local variables
+        integer  :: i, j, nx, ny, n, n2 
+        real(wp), allocatable :: filter0(:,:), filter(:,:) 
+        real(wp), allocatable :: var_old(:,:) 
+        logical,  allocatable :: mask_apply_local(:,:) 
+        logical,  allocatable :: mask_use_local(:,:) 
+
+        nx    = size(var,1)
+        ny    = size(var,2)
+
+        ! Determine half-width of filter as 2-sigma
+        n2 = ceiling(2.0_wp*sigma / dx)
+
+        ! Get total number of points for filter window in each direction
+        n = 2*n2+1
+        
+        allocate(var_old(nx,ny))
+        allocate(mask_apply_local(nx,ny))
+        allocate(mask_use_local(nx,ny))
+        allocate(filter0(n,n))
+        allocate(filter(n,n))
+
+        ! Check whether mask_apply is available 
+        if (present(mask_apply)) then 
+            ! use mask_use to define neighborhood points
+            
+            mask_apply_local = mask_apply 
+
+        else
+            ! Assume that everywhere should be smoothed
+
+            mask_apply_local = .TRUE.
+        
+        end if
+
+        ! Check whether mask_use is available 
+        if (present(mask_use)) then 
+            ! use mask_use to define neighborhood points
+            
+            mask_use_local = mask_use 
+
+        else
+            ! Assume that mask_apply also gives the points to use for smoothing 
+
+            mask_use_local = mask_apply_local
+        
+        end if
+
+        ! Calculate default 2D Gaussian smoothing kernel
+        filter0 = gauss_values(dx,dx,sigma=sigma,n=n)
+
+        var_old = var 
+
+        do j = n2+1, ny-n2
+        do i = n2+1, nx-n2
+
+            if (mask_apply_local(i,j)) then 
+                ! Apply smoothing to this point 
+
+                ! Limit filter input to neighbors of interest
+                filter = filter0 
+                where(.not. mask_use_local(i-n2:i+n2,j-n2:j+n2)) filter = 0.0
+
+                ! If neighbors are available, normalize and perform smoothing  
+                if (sum(filter) .gt. 0.0) then 
+                    filter = filter/sum(filter)
+                    var(i,j) = sum(var_old(i-n2:i+n2,j-n2:j+n2)*filter) 
+                end if  
+
+            end if 
+
+        end do 
+        end do 
+
+        return 
+
+    end subroutine smooth_gauss_2D
+
+    function gauss_values(dx,dy,sigma,n) result(filt)
+        ! Calculate 2D Gaussian smoothing kernel
+        ! https://en.wikipedia.org/wiki/Gaussian_blur
+
+        implicit none 
+
+        real(wp), intent(IN) :: dx 
+        real(wp), intent(IN) :: dy 
+        real(wp), intent(IN) :: sigma 
+        integer,  intent(IN) :: n 
+        real(wp) :: filt(n,n) 
+
+        ! Local variables 
+        real(wp) :: x, y  
+        integer  :: n2, i, j, i1, j1  
+
+        if (mod(n,2) .ne. 1) then 
+            write(*,*) "gauss_values:: error: n can only be odd."
+            write(*,*) "n = ", n 
+        end if 
+
+        n2 = (n-1)/2 
+
+        do j = -n2, n2 
+        do i = -n2, n2 
+            x = i*dx 
+            y = j*dy 
+
+            i1 = i+1+n2 
+            j1 = j+1+n2 
+            filt(i1,j1) = 1.0/(2.0*pi*sigma**2)*exp(-(x**2+y**2)/(2*sigma**2))
+
+        end do 
+        end do 
+        
+        ! Normalize to ensure sum to 1
+        filt = filt / sum(filt)
+
+        return 
+
+    end function gauss_values
 
     ! === ISOS physics routines ======================================
 
